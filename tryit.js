222a3332bffa077a3763d13776c1d8351b404e3a
Serhii <botodnepr@gmail.com>
2019-01-31 16:40:24 +0200

    tests: wait travis detect my changes

--- tryit.js
+++ tryit.js
@@ -4,4 +4,3 @@ const session = 'eyJwYXNzcG9ydCI6eyJ1c2VyIjoiNWMzNDlhM2VhNThlNzAyOGMzNzYzNjQ5In1
 const keygrip = new Keygrip(['123123123'])
 const hash = keygrip.sign('session=' + session)
 
-console.log(hash)
